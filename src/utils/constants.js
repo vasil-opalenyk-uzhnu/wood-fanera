a2758c7774d7562d46a71650c1a1aa19eb0f9687
Vasil_Opalenyk <vasil_opalenyk@MacBook-Air-Vasil-Opalenyk.local>
2025-12-03 20:40:52 +0200

    Add relative path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,26 +6,26 @@ export const LINKS = [
 ];
 
 export const PRODUCTS = [
-    { img: "/img/products/chair/chair-grande.png", rating: 5, title: "Chair Grande", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 100, type: "chair" },
-    { img: "/img/products/chair/chair-jim.png", rating: 5, title: "Chair Jim", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 80, type: "chair" },
-    { img: "/img/products/chair/chair-trenk.png", rating: 5, title: "Chair Trenk", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "chair" },
-    { img: "/img/products/cutting-board/cutting-board-24x15.png", rating: 5, title: "Cutting board 24x15", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "cutting-board" },
-    { img: "/img/products/cutting-board/cutting-board-27x19.png", rating: 5, title: "Cutting board 27x19", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "cutting-board" },
-    { img: "/img/products/cutting-board/cutting-board-29x19.png", rating: 5, title: "Cutting board 29x19", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "cutting-board" },
-    { img: "/img/products/table/table-alpha.png", rating: 5, title: "Table alpha", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
-    { img: "/img/products/table/table-battle.png", rating: 5, title: "Table battle", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
-    { img: "/img/products/table/table-fort.png", rating: 5, title: "Table fort", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
-    { img: "/img/products/table/table-oldi.png", rating: 5, title: "Table oldi", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
-    { img: "/img/products/table/table-oscar.png", rating: 5, title: "Table oscar", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
-    { img: "/img/products/toothbrush/toothbrush-adult.png", rating: 5, title: "Toothbrush adult", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "toothbrush" },
-    { img: "/img/products/toothbrush/toothbrush-cs-wood.png", rating: 5, title: "Toothbrush cs wood", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "toothbrush" },
+    { img: "img/products/chair/chair-grande.png", rating: 5, title: "Chair Grande", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 100, type: "chair" },
+    { img: "img/products/chair/chair-jim.png", rating: 5, title: "Chair Jim", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 80, type: "chair" },
+    { img: "img/products/chair/chair-trenk.png", rating: 5, title: "Chair Trenk", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "chair" },
+    { img: "img/products/cutting-board/cutting-board-24x15.png", rating: 5, title: "Cutting board 24x15", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "cutting-board" },
+    { img: "img/products/cutting-board/cutting-board-27x19.png", rating: 5, title: "Cutting board 27x19", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "cutting-board" },
+    { img: "img/products/cutting-board/cutting-board-29x19.png", rating: 5, title: "Cutting board 29x19", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "cutting-board" },
+    { img: "img/products/table/table-alpha.png", rating: 5, title: "Table alpha", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
+    { img: "img/products/table/table-battle.png", rating: 5, title: "Table battle", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
+    { img: "img/products/table/table-fort.png", rating: 5, title: "Table fort", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
+    { img: "img/products/table/table-oldi.png", rating: 5, title: "Table oldi", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
+    { img: "img/products/table/table-oscar.png", rating: 5, title: "Table oscar", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "table" },
+    { img: "img/products/toothbrush/toothbrush-adult.png", rating: 5, title: "Toothbrush adult", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "toothbrush" },
+    { img: "img/products/toothbrush/toothbrush-cs-wood.png", rating: 5, title: "Toothbrush cs wood", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.", cost: 60, type: "toothbrush" },
 ]
 
 export const CONTACTS = [
-    {img: "/img/icons/mail.svg", text: "example@gamil.com"},
-    {img: "/img/icons/viber.svg", text: "viber nik name"},
-    {img: "/img/icons/telegram.svg", text: "@example"},
-    {img: "/img/icons/whats-up.svg", text: "+380 99 999 99 99"},
+    {img: "img/icons/mail.svg", text: "example@gamil.com"},
+    {img: "img/icons/viber.svg", text: "viber nik name"},
+    {img: "img/icons/telegram.svg", text: "@example"},
+    {img: "img/icons/whats-up.svg", text: "+380 99 999 99 99"},
 ]
 
 export const CATEGORIES = ["chair", "table", "toothbrush", "cutting-board"]
